8612eeca8f3816adfb3558b2f8df55597b855d26
Stefan Wienert <info@stefanwienert.de>
2016-11-17 16:09:38 +0100

    Added weather and better styling

---
+++ src/pkjs/index.js
@@ -0,0 +1,57 @@
+
+var OPENWEATHER_APIKEY = "a06e04c571698b278c625d94db0812f6";
+
+Pebble.on('message', function(event) {
+  // Get the message that was passed
+  var message = event.data;
+
+  if (message.fetch) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      var url = 'http://api.openweathermap.org/data/2.5/weather' +
+              '?lat=' + pos.coords.latitude +
+              '&lon=' + pos.coords.longitude +
+              '&units=metric&lang=de' +
+              '&appid=' + OPENWEATHER_APIKEY;
+      
+      log("Weather url: " + url);
+
+      request(url, 'GET', function(respText) {
+        log("Response from weather API:");
+        log(respText);
+        var weatherData = JSON.parse(respText);
+        Pebble.postMessage({
+          'weather': {
+            'windSpeed': Math.round(weatherData.wind.speed * 3.6),
+            'windDeg': weatherData.wind.deg,
+            'humidity': weatherData.main.humidity, // percent
+            'tempMin': weatherData.main.temp_min,
+            'tempMax': weatherData.main.temp_max,
+            'sunrise': weatherData.sys.sunrise, // Unix epoch
+            'sunset': weatherData.sys.sunset,
+            'location': weatherData.name,
+            'temp': Math.round(weatherData.main.temp),
+            
+            'visibility': weatherData.visibility / 1000, // Visibility in km
+            'desc': weatherData.weather[0].description
+          }
+        });
+      });
+    }, function(err) {
+      console.error('Error getting location');
+    },
+    { timeout: 15000, maximumAge: 60000 });
+  }
+});
+
+function request(url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+}
+
+function log(msg) {
+  console.log(msg);
+}
--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,6 +1,12 @@
 var rocky = require('rocky');
 
 var Config = {
+  hourTicks: {
+    width: 2,
+    length: 3,
+    widthQuarter: 2,
+    lengthQuarter: 3
+  },
   hourHand: {
     width: 3,
     length: 0.5,
@@ -8,26 +14,37 @@ var Config = {
     color: "white"
   },
   minuteHand: {
-    width: 2,
+    width: 4,
     length: 0.85,
     distanceFromCenter: 0.05,
-    color: "white"
+    color: "lightgray"
+  },
+  weather: {
+    windArrow: {
+      length: 7,
+      tipAngle: 35,
+      tipLength: 3
+    }
   }
 };
+
+var State = {
+  weather: null,
+  weatherAge: 0,
+  // center of screen
+  cx: null,
+  cy: null
+};
+
 
 function fractionToRadian(fraction) {
   return fraction * 2 * Math.PI;
 }
-
-function drawHand(ctx, angle, config) {
-  // Find the end points
-  var x2 = Config.cx + Math.sin(angle) * config.length * Config.maxLength;
-  var y2 = Config.cy - Math.cos(angle) * config.length * Config.maxLength;
-
-  var x1 = Config.cx + Math.sin(angle) * (config.distanceFromCenter * Config.maxLength);
-  var y1 = Config.cy - Math.cos(angle) * (config.distanceFromCenter * Config.maxLength);
-  drawLine(ctx, x1, y1, x2, y2, config.width, config.color);
+function degree2rad(degree) {
+  return degree * Math.PI / 180;
 }
+
+
 
 rocky.on('draw', function(event) {
 
@@ -41,30 +58,97 @@ rocky.on('draw', function(event) {
   var w = ctx.canvas.unobstructedWidth;
   var h = ctx.canvas.unobstructedHeight;
 
-  // Determine the center point of the display
-  // and the max size of watch hands
-  Config.cx = w / 2;
-  Config.cy = h / 2;
-
-  // -20 so we're inset 10px on each side
-  Config.maxLength = (Math.min(w, h) - 20) / 2;
+  State.cx = w / 2;
+  State.cy = h / 2;
+  Config.maxLength = (Math.min(w, h) - 10) / 2;
   
   drawHands(ctx, d);
-
-  
+  drawCenter(ctx);
   for(var i=0; i < 12; i++) {
     drawHourMark(ctx, i);
   }
+  drawDate(ctx, formatDate(d));
 
-  var dateString = formatDate(d);
+  // draw weather if not older than 2 hours
+  if(State.weather && State.weatherAge > (d.getTime() / 1000 - 7200)){
+    drawWeather(ctx, State.weather);
+  }
+});
+
+function drawCenter(ctx) {
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'white';
+  ctx.beginPath();
+  ctx.arc(State.cx, State.cy, 2, 0, 2 * Math.PI, false);
+  ctx.stroke(); 
+}
+
+function drawDate(ctx, dateString) {
   ctx.fillStyle = 'white';
   ctx.font = '14px Gothic';
   // Center align the text
-  ctx.textAlign = 'center';
+  ctx.textAlign = 'right';
   // Display the time, in the middle of the screen
-  ctx.fillText(dateString, Config.cx + w / 4, h - 20, (w / 2) - 5);
-  //drawDate(ctx, d);
-});
+  ctx.fillText(dateString, State.cx * 2 - 5, (State.cy * 2) - 20, State.cx - 5);
+}
+
+function drawHand(ctx, angle, config) {
+  // Find the end points
+  var x2 = State.cx + Math.sin(angle) * config.length * Config.maxLength;
+  var y2 = State.cy - Math.cos(angle) * config.length * Config.maxLength;
+
+  var x1 = State.cx + Math.sin(angle) * (config.distanceFromCenter * Config.maxLength);
+  var y1 = State.cy - Math.cos(angle) * (config.distanceFromCenter * Config.maxLength);
+  drawLine(ctx, x1, y1, x2, y2, config.width, config.color);
+}
+
+function drawWeather(ctx, weather) {
+  ctx.fillStyle = 'white';
+  ctx.font = '14px Gothic';
+  ctx.textAlign = 'left';
+  var string = "" + weather.temp + "°C " + weather.desc;
+  ctx.fillText(string, 5, 2, (State.cx - 5));  
+  ctx.textAlign = 'right';
+  ctx.fillStyle = 'lightgray';
+  string = "" + weather.tempMin + "°C/"  + weather.tempMax + "°C";
+  ctx.fillText(string, State.cx * 2 - 5, 2, (State.cx - 5));  
+
+  //var deg = weather.windDeg;
+  
+  var deg = weather.windDeg - 90; // Offset; Wind rose 0 degree is north, while math 0 degree is x axis
+  if(deg < 0) {
+    deg = deg + 360;
+  }
+  var windAngle = degree2rad(deg);
+  var arrowLength = Config.weather.windArrow.length;
+  
+  var dx = arrowLength * Math.cos(windAngle);
+  var dy = arrowLength * Math.sin(windAngle);
+  var x1 = 10;
+  var y1 = round(State.cy * 2 - 10);
+  var x2 = round(x1 + dx);
+  var y2 = round(y1 + dy);
+  drawLine(ctx, x1, y1, x2, y2, 1, 'white');  
+ 
+  function drawArrowTip(ctx, x1, x2, tipAngle) {
+    var C = Config.weather.windArrow;
+    var arrowDegree = degree2rad(tipAngle);
+    var arrowX = Math.cos(arrowDegree) * C.tipLength;
+    var arrowY = Math.sin(arrowDegree) * C.tipLength;
+    var x3 = round(x2 + arrowX);
+    var y3 = round(y2 + arrowY);
+    drawLine(ctx, x2, y2, x3, y3, 1, 'white');     
+  }
+  drawArrowTip(ctx, x1, x2, deg + 180 - Config.weather.windArrow.tipAngle);
+  drawArrowTip(ctx, x1, x2, deg + 180 + Config.weather.windArrow.tipAngle);
+  ctx.textAlign = 'left';
+  ctx.fillText("" + weather.windSpeed + "km/h", x1 + 10, (State.cy * 2) - 20, (State.cx - 10));
+}
+
+function round(number) {
+  return Math.round(number * 10) / 10;
+}
+
 
 function formatDate(d) {
   // TODO d.toLocaleString is broken in emulator, so manual
@@ -84,12 +168,6 @@ function drawHands(ctx, d) {
   var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
   var hourAngle = fractionToRadian(hourFraction);
   drawHand(ctx, hourAngle, Config.hourHand);
-  drawHand(ctx, hourAngle, {
-    width: 1,
-    length: 0.48,
-    distanceFromCenter: 0.05,
-    color: "darkgray"
-  });
 }
 
 
@@ -98,16 +176,17 @@ function drawHourMark(ctx, i) {
   var angle = fractionToRadian(pos);
   var length, width;
   if (i % 3 === 0) {
-    length = 2;
-    width = 2;
+    length = Config.hourTicks.widthQuarter;
+    width = Config.hourTicks.lengthQuarter;
   } else {    
-    length = 1;
-    width = 1;
+    length = Config.hourTicks.width;
+    width = Config.hourTicks.length;
   }
-  var x1 = Config.cx + Math.sin(angle) * Config.maxLength;
-  var y1 = Config.cy - Math.cos(angle) * Config.maxLength;
-  var x2 = Config.cx + Math.sin(angle) *  (Config.maxLength + length );
-  var y2 = Config.cy - Math.cos(angle) *  (Config.maxLength + length );
+  var radius = Config.maxLength * 0.9;
+  var x1 = State.cx + Math.sin(angle) * radius;
+  var y1 = State.cy - Math.cos(angle) * radius;
+  var x2 = State.cx + Math.sin(angle) *  (radius + length );
+  var y2 = State.cy - Math.cos(angle) *  (radius + length );
   drawLine(ctx, x1, y1, x2, y2, width, "lightgray");
 
 }
@@ -124,4 +203,18 @@ function drawLine(ctx, x1, y1, x2, y2, lineWidth, color) {
 rocky.on('minutechange', function(event) {
   // Request the screen to be redrawn on next pass
   rocky.requestDraw();
+});
+
+rocky.on("hourchange", function(event) {
+  rocky.postMessage({'fetch': true});
+});
+
+rocky.on('message', function(event) {
+  var message = event.data;
+  if (message.weather) {
+    
+    State.weather = message.weather;
+    State.weatherAge = new Date().getTime() / 1000;
+    rocky.requestDraw();
+  }
 });
